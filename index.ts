0584ce3d30f210d273a19ed3b3b647508810c2f3
Alexandre Seyffert <alexseyf66@gmail.com>
2025-04-06 09:45:49 -0300

    remove deploy configs

--- index.ts
+++ index.ts
@@ -11,14 +11,7 @@ const app = express()
 const port = 3001
 
 app.use(express.json())
-app.use(cors(
-  {
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Access-Control-Allow-Origin'], 
-    exposedHeaders: ['Authorization', 'Access-Control-Allow-Origin'],
-  }
-))
+app.use(cors())
 
 app.use("/admins", routesAdmins)
 app.use("/suporte", routesSuporte)
@@ -31,28 +24,8 @@ app.get('/', (req, res) => {
   res.send('API - ALUGUEL DE IMÓVEIS')
 })
 
-app.use((req, res, next) => {
-  next()
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta: ${port}`)
 })
-
-app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
-  console.error('Error:', err)
-  console.error('Stack:', err.stack)
-  res.status(500).json({ 
-    error: 'Algo deu errado!',
-    details: err.message,
-    stack: process.env.NODE_ENV === 'development' ? err.stack : undefined
-  })
-})
-
-// app.listen(port, () => {
-//   console.log(`Servidor rodando na porta: ${port}`)
-// })
-
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3001')
-  })
-}
 
 export default app
